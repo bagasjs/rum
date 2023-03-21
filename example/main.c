9f780fc1ab17f66c3f1cf02a71ec47cb5ba6e854
thankz4lot <thankz4lot@gmail.com>
2023-03-22 04:26:33 +0700

    Image shown: Finished

--- example/main.c
+++ example/main.c
@@ -7,10 +7,10 @@ int main(void) {
 	if(!rum_init("Example", 640, 480))
 		return 1;
 	int w, h, channels;
-	stbi_set_flip_vertically_on_load(true);
+	// stbi_set_flip_vertically_on_load(true);
 	unsigned char* data = stbi_load("hxbunny.jpg", &w, &h, &channels, 4);
 	while(!rum_check_event(RUM_EVENT_QUIT)) {
-		rum_copy_image(channels == 4 ? RUM_RGBA : RUM_RGB, data, w, h, 0, 0);
+		rum_copy_image(channels == 4 ? RUM_RGBA : RUM_RGB, data, w, h, 100, 0);
 		rum_update_screen();
 	}
 	rum_terminate();
